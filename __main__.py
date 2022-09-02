45053942bddb1d5dd4941c9f73057b5c3d28d668
mdoo <@..>
2022-09-02 13:53:38 -0400

    .

--- __main__.py
+++ __main__.py
@@ -2,15 +2,13 @@ import random
 #  import maya.cmds as cmds
 
 
-rng = [1, 2]
-print(rng)
+def setRotate():
+    temptype = random.choice(range(1, 3))
 
-temp = random.choice(rng)
+        if temptype == 1:
+            setrotatevec = [0, 90, 0]
+        else:
+            setrotatevec = [0, 0, 0]
 
-if temp == 1:
-    retnrt = [0, 90, 0]
+        return setrotatevec
 
-if temp == 2:
-    retnrt = [0, 0, 0]
-
-print(retnrt)
