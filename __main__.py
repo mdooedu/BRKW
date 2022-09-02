a2dd49250e48d65d2e874100b920dbb8832cb617
mdoo <@..>
2022-09-02 13:45:30 -0400

    .

--- __main__.py
+++ __main__.py
@@ -1,9 +1,16 @@
 import random
+#  import maya.cmds as cmds
 
 
-rng = [1, 2, 3]
+rng = [1, 2]
 print(rng)
 
 temp = random.choice(rng)
 
-print(temp)
+if temp == 1:
+    retnrt = [0, 90, 0]
+
+if temp == 2:
+    retnrt = [0, 0, 0]
+
+print(retnrt)
