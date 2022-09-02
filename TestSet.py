53a69fe019a2a9434295b7c8e98eaf45255b5eff
mdoo <@..>
2022-09-02 14:06:59 -0400

    .

--- TestSet.py
+++ TestSet.py
@@ -1,8 +1,12 @@
 import random
 import maya.cmds as cmds
 
-tempvec = [0, 90, 0]
-temp = 0
-i = temp + 1
 
-cmds.setAttr("BRK" + i + ".rotate")
+targetcount = int(input("targetcount"))
+
+for ctemp in targetcount:
+    tempvec = [0, 90, 0]
+    temp = 0
+    i = temp + 1
+
+#    cmds.setAttr("BRK" + i + ".rotate")
